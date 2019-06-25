53d695a37d33f1aa734f653fabe6cb4ba6d12cd5
jkeary <jkeary@gmail.com>
2019-06-25 14:14:30 -0400

    about me layout

--- aboutme.php
+++ aboutme.php
@@ -17,11 +17,11 @@
         <div>
           <p class="lead">
             <img class="rightimg_lg img-responsive img-rounded" src="./imgs/frend.jpg" align="right"/>
-            For the past 5 years I've been working as a Software Engineer for the Washington D.C. advocacy marketing start-up Crowdskout.   Here is my current <a href="./docs/Resume2019.pdf" align="right"/>software engineering resume</a> with details on what I accomplished while there.
+            I am a musician and a songwriter.  My current project is the DC to Philadelphia rock band <a href="https://frendtheband.bandcamp.com" target="_blank">Frend</a>.  Previously I was a member of the NYC indie rock group <a href="https://miniboone.bandcamp.com" target="_blank">Miniboone</a>.  I have been commissioned to do pieces for commercials and videos, produced and written songs for and with other artists, created multiple recordings and music videos of my own work, toured and played over 200 shows, performed at major music festivals, and even have had songs I've written placed in films and tv shows.  My current <a href="./docs/CV.pdf" align="right"/>CV</a> has a complete listing of those credits.
             <br><br>
-            I am also a musician/songwriter.  Prior to my work at Crowdskout, I attended New York University, Steinhardt, where I received my <a href="http://steinhardt.nyu.edu/music/technology/programs/graduate/" target="_tab">Masters in Music, Music Technology degree</a>.  I studied Digital Signal Processing, Audio Programming, Audio Engineering and Composition.  I also worked as a Research Assistant and Programmer on a <a href="./sonification_proj.php">Medical Sonification Project</a> in conjunction with the NYU Langone Medical Center.
+            For the past 5 years I worked as a Software Engineer for the Washington D.C. political advocacy marketing start-up Crowdskout.  Here is my current <a href="./docs/SoftwareEngineerResume.pdf" align="right"/>software engineering resume</a> with details on what I accomplished in that field.
             <br><br>
-              My current project is the DC Rock band <a href="https://frendtheband.bandcamp.com" target="_blank">Frend</a>.  Previously I was a founding member of the NYC Rock group <a href="https://miniboone.bandcamp.com" target="_blank">Miniboone</a>, several commissioned pieces for commercials and video, produced and written songs for other musical artists, and even have had songs placed in film and video.  
+            I also have work and education experience in the audio tech field.  I went to NYU Steinhart where I received my Masters in Music Technology.  Here is my current <a href="./docs/AudioEngineerResume.pdf" align="right"/>audio engineering resume</a> with my accomplished in that line of work.
             <br><br>
             Feel free to contact me at <a href="mailto:jkeary@gmail.com" target="_blank">jkeary@gmail.com</a>.
           </p> 
